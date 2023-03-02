d35e844425e4fc47d260f6e32152f0c68185234d
miku0701 <miku.0817m@icloud.com>
2023-03-02 17:27:06 +0900

    resources

---
+++ resources/views/photos/show.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.default')
+
+@section('title','画像アップロード')
+@section('content')
+    @if(session()->has('success'))
+        <p>{{ session('success') }}</p>
+    @endif
+
+    <img src="{{ asset('storage/photos/'. $fileName) }}" alt="">
+
+    <form action="{{ route('photos.destroy',['photo' => $fileName]) }}" method="POST">
+    @csrf
+    @method('DELETE')
+    <button type="submit">削除</button>
+    </form>
+@endsection
