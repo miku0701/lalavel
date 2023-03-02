1b95b50c497a55a9365a79fc2e32dd2bef0e580a
miku0701 <miku.0817m@icloud.com>
2023-03-02 16:26:50 +0900

    first

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class EventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * 登録画面表示用
+     */
+    public function create()
+    {
+        return view('events.create');
+    }
+
+    /**
+     * 登録処理・登録画面表示用
+     */
+    public function store(Request $request)
+    {
+        $title = $request->get('title');
+        Log::debug('イベント名:'. $title);
+        return to_route('events.create')->with('success', $title. 'を登録しました');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     *  @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
